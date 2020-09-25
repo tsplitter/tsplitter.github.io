ea39e20f89923846f6248c753d11b2abb6e0b276
tsplitter <pa.tsplitter@yahoo.com>
2020-09-26 00:46:47 +0100

    Completed module

---
+++ ssg/hooks.py
@@ -0,0 +1,20 @@
+_callbacks = {}
+
+def register(hook, order=0):
+    def register_callback(self):
+        _callbacks.setdefault(hook, {}).append(func)
+        return self.register_callback()
+    return register_callback()
+
+def event(hook, *args):
+    for order in sorted(_callbacks.get(hook, {})):
+        for func in _callbacks[hook][order]:
+            event(*args)
+
+def filter(hook, value, *args):
+    for order in sorted(_callbacks.get(hook, {})):
+        for func in _callbacks[hook][order]:
+            event(value, *args) = value
+    return value
+
+
