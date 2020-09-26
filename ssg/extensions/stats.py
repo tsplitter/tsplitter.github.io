8bfd51781142408fd3d404d1d9ec82c2278e0f8d
tsplitter <pa.tsplitter@yahoo.com>
2020-09-26 02:27:17 +0100

    Completed module

--- ssg/extensions/stats.py
+++ ssg/extensions/stats.py
@@ -6,7 +6,8 @@ total_written = 0
 
 @hooks.register("start_build")
 def start_build():
-    global start_time = time.time()
+    global start_time
+    start_time = time.time()
 
 @hooks.register("written")
 def written():
